7d88ea9e5c04d85b730244019149951bfbaef18f
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2022-12-21 18:09:55 +0700

    Update all_urls.py

--- apps/common/all_urls.py
+++ apps/common/all_urls.py
@@ -1,20 +1,21 @@
 def all_urls_view():
     from api.urls import urlpatterns
-    return get_urls(urlpatterns, [])
+    return get_urls(urlpatterns, {})
 
 
-def get_urls(raw_urls, nice_urls=[]):
+def get_urls(raw_urls, nice_urls={}):
     for entry in raw_urls:
         if hasattr(entry, 'url_patterns'):
             get_urls(entry.url_patterns, nice_urls)
         else:
             if hasattr(entry.callback, 'actions'):
-                nice_urls.extend([
-                    {
-                        "pattern": entry.pattern.regex.pattern,
-                        "action": _action,
-                        'method': _method,
-                        'func': entry.lookup_str
-                    } for (_method, _action) in entry.callback.actions.items()
-                ])
-    return nice_urls
+                for (_method, _action) in entry.callback.actions.items():
+                    if f'{entry.lookup_str}_{_action}' not in nice_urls:
+                        nice_urls[f'{entry.lookup_str}_{_action}'] = {
+                            "pattern": entry.pattern.regex.pattern,
+                            "action": _action,
+                            'method': _method,
+                            'func': entry.lookup_str,
+                            'meta': f'{entry.lookup_str}_{_action}'
+                        }
+    return nice_urls.values()
