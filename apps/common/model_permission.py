0d2f18ebfa9439ce184468bde04ca16ff9a13039
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2022-12-21 17:51:30 +0700

    Update model_permission.py

--- apps/common/model_permission.py
+++ apps/common/model_permission.py
@@ -2,16 +2,16 @@ from rest_framework.permissions import BasePermission
 
 
 class GenericApiViewPermission(BasePermission):
-    message = "You do not have permission to perform action"
-    permission_code = '{app_label}.{api_view}_{action}'
+    APP_LABEL = 'apps'
+    message = 'You do not have permission to perform action'
+    permission_code = '{app_label}.{func}_{action}'
 
-    def _get_permission(self, action, perm_slug):
-        app, api_view = perm_slug.split(".")
-        perm = self.permission_code.format(app_label=app, api_view=api_view, action=action)
+    def _get_permission(self, action, func):
+        perm = self.permission_code.format(app_label=self.APP_LABEL, func=func, action=action)
         return perm
 
     def has_permission(self, request, view):
-        perm = self._get_permission(action=view.action, perm_slug=view.perm_slug)
+        perm = self._get_permission(action=view.action, func=resolve(request.path)._func_path)
         if request.user.has_perm(perm):
             return True
         return False
