2783de4287e5c87ce74d30f6ae0005c1b49eb395
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2023-01-18 11:07:19 +0700

    Update urls.py

--- apps/sample_els/urls.py
+++ apps/sample_els/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from apps.sample_els.views import SampleDocumentApiView
 
 router = routers.DefaultRouter()
-router.register('sample-esl', SampleDocumentApiView, basename='sample-esl')
+router.register('sample-els', SampleDocumentApiView, basename='sample-els')
 
 urlpatterns = router.urls
