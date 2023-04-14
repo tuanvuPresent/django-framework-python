9c66465f647c357c183c8da2ab5866affbf67f76
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2023-04-14 17:48:10 +0700

    Update settings.py

--- Example/settings.py
+++ Example/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'silk.middleware.SilkyMiddleware',
+    'apps.common.middleware.LogRequest',
 ]
 
 # Cors
