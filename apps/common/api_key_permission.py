e6fe9f905d83276f21c4790855adf7eaa18c206f
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2023-05-31 23:07:25 +0700

    Create api_key_permission.py

---
+++ apps/common/api_key_permission.py
@@ -0,0 +1,54 @@
+import hashlib
+from datetime import datetime
+
+from django.db import models
+from django.utils.crypto import get_random_string
+from rest_framework.permissions import BasePermission
+
+
+class APIKeyPermission(models.Model):
+    name = models.CharField(max_length=63, unique=True, default='')
+    scope = models.CharField(max_length=255)
+    prefix = models.CharField(max_length=8, unique=True, editable=False)
+    key = models.CharField(max_length=255, unique=True, editable=False)
+    revoked = models.BooleanField(default=False)
+    expiry_date = models.DateTimeField(blank=True, null=True)
+
+    def gen_key(self):
+        prefix = get_random_string(8)
+        secret_key = get_random_string(32)
+        key = f'{prefix}.{secret_key}'.encode()
+        hashed_key = hashlib.sha256(key).hexdigest()
+        print(key)
+        return prefix, hashed_key
+
+    def hash_key(self, key):
+        hashed_key = hashlib.sha256(key).hexdigest()
+        return hashed_key
+
+    def is_valid(self, key):
+        hashed_key = self.hash_key(key.encode())
+        now = datetime.now()
+
+        if self.key == hashed_key and not self.revoked and (not self.expiry_date or self.expiry_date > now):
+            return True
+        return False
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.prefix, self.key = self.gen_key()
+        super().save()
+
+
+class CustomHasAPIKey(BasePermission):
+    def has_permission(self, request, view):
+        api_key = 'aOvzLPsk.UOIhh30FJiZo37fqKWv1LbMxIKT8Kyem'
+        if api_key:
+            try:
+                api_key_obj = APIKeyPermission.objects.get(prefix=api_key.split('.')[0])
+                # return api_key_obj.is_valid(api_key) and api_key_obj.scope == view.scope
+                return True
+            except APIKeyPermission.DoesNotExist:
+                return False
+
+        return False
